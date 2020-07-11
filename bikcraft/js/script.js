8ca02be545285930cd6ad34d7722aae5802a2146
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-07-11 20:47:55 -0300

    Inclusão dos slides na pagina interna de portfólio.

--- bikcraft/js/script.js
+++ bikcraft/js/script.js
@@ -1,3 +1,13 @@
 document.documentElement.classList.add('js')
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote", // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+  });
 
-new SimpleAnime()
+  new SimpleSlide({
+    slide: "portfolio", // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    nav: true,
+  });
+}
